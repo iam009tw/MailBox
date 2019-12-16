d2a12c065bf5264cb83cf7164cab8e61e71c91d4
kamichen <iamkami69@gmail.com>
2019-12-16 17:05:14 +0800

    作業1 - 建立一個新的 Layout resource file

---
+++ app/src/main/java/com/kami/mailbox/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kami.mailbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_box)
+    }
+}
